1e777aff823556ed789cacf6c8efa5f53edb946d
Vincenzo <61056419+Sbi98@users.noreply.github.com>
2021-04-01 18:17:23 +0200

    synch

--- Node/src/it/polimi/ds/ricciosorrentinotriuzzi/snaptest/PublicInt.java
+++ Node/src/it/polimi/ds/ricciosorrentinotriuzzi/snaptest/PublicInt.java
@@ -1,7 +1,5 @@
 package it.polimi.ds.ricciosorrentinotriuzzi.snaptest;
 
-import org.apache.commons.configuration.ConfigurationException;
-
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
@@ -10,6 +8,7 @@ public interface PublicInt extends Remote {
     void printStr(String toPrint) throws RemoteException;
     void transfer(String to, Integer amount) throws RemoteException;
     void withdraw(String from, Integer amount) throws RemoteException;
+    void register(String customer) throws RemoteException;
     void addConn(boolean toOutgoing, String host, int port, String name) throws RemoteException;
     void removeConn(boolean fromOutgoing, String host) throws RemoteException;
 }
